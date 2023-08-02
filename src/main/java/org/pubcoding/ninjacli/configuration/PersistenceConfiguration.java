bb17bc66a7e84d0e6b123deb989f36e53efd2aeb
Marco Riggio <marco.riggio1994@gmail.com>
2023-08-02 07:49:19 +0200

    change pc

--- src/main/java/org/pubcoding/ninjacli/configuration/PersistenceConfiguration.java
+++ src/main/java/org/pubcoding/ninjacli/configuration/PersistenceConfiguration.java
@@ -5,13 +5,19 @@ import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
-public class PersistenceConfiguration implements RuntimeHintsRegistrar {
+@ImportRuntimeHints(PersistenceConfiguration.SQLiteStrategyRegistrar.class)
+public class PersistenceConfiguration {
 
-    @Override
-    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        hints.reflection().registerType(SQLiteDialect.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+    static class SQLiteStrategyRegistrar implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection().registerType(SQLiteDialect.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+
+        }
     }
 
 }
